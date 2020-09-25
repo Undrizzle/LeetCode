dde81bfe38b1a00a033d661c6c56ae19ea67fc5d
undrizzle <undrizzle@gmail.com>
2020-09-25 13:47:09 +0800

    387. 字符串中的第一个唯一字符

--- string.playground/Contents.swift
+++ string.playground/Contents.swift
@@ -12,4 +12,21 @@ class Solution {
             s[i] = ch
         }
     }
+    
+    /*
+     字符串中的第一个唯一字符
+     "leetcode" -> 0
+     */
+    func firstUniqChar(_ s: String) -> Int {
+        var dict = [String.Element: Int]()
+        for ch in s {
+            dict[ch, default: 0] += 1
+        }
+        for (idx, c) in s.enumerated() {
+            if dict[c] == 1 {
+                return idx
+            }
+        }
+        return -1
+    }
 }
